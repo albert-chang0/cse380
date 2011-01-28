e02ec76099c51aad516397493f84470dc525b2e5
Albert Chang <albert.chang@gmx.com>
2011-01-28 09:25:09 -0500

    Adds the hamming code portion (untested)

--- lab2/lab2skeleton.s
+++ lab2/lab2skeleton.s
@@ -5,15 +5,77 @@
 hamming
         stmfd r13!, {r1-r12, r14}
 
-        ; Your code for the hamming routine goes here.  
-        ; The argument (8-bit hamming code) is passed in r0. 
-        ; The corrected data (or -1 if uncorrectable) is returned in r0.
+        ; r0 - code
+        ; r1 - bit 3 (1st digit)
+        ; r2 - bit 5 (2nd digit)
+        ; r3 - bit 6 (3rd digit)
+        ; r4 - bit 7 (4th digit)
+        ; r5 - checksum
+        ; r6 - temp
+        ; r7 - correctable boolean
 
-        ldmfd r13!, {r1-r12, r14}
+        mov r7, #0 ; assume it's correctable
+
+        ; first isolate bits
+        bic r1, r0, #0xfb ; clear all bits besides the 3rd one
+        eor r7, r7, r1, lsr #2
+        bic r2, r0, #0xef ; clear all bits besides the 5th one
+        eor r7, r7, r2, lsr #4
+        bic r3, r0, #0xdf ; clear all bits besides the 6th one
+        eor r7, r7, r3, lsr #5
+        bic r4, r0, #0xbf ; clear all bits besides the 7th one
+        eor r7, r7, r4, lsr #6
+
+        ; boolean operations
+        ; calculate checksum with the first parity bit
+        bic r5, r0, #0xfe
+        eor r7, r7, r5
+        eor r5, r5, r1, lsr #1
+        eor r5, r5, r2, lsr #4
+        eor r5, r5, r4, lsr #6
+        ; calculate checksum with the second parity bit
+        bic r6, r0, #0xfd
+        eor r7, r7, r6, lsr #1
+        eor r6, r6, r1, lsr #1
+        eor r6, r6, r3, lsr #4
+        eor r6, r6, r4, lsr #5
+        add r5, r5, r6
+        ; calculate checksum with the third parity bit
+        bic r6, r0, #0xf7
+        eor r7, r7, r6, lsr #3
+        eor r6, r6, r2, lsr #1
+        eor r6, r6, r3, lsr #2
+        eor r6, r6, r4, lsr #3
+        add r5, r5, r6, lsr #1
+
+        cmp r5, #0
+        beq recon
+
+        ; is it correctable?
+        cmp r7, #0
+        bne crct
+
+        mov r0, #-1
+
+done    ldmfd r13!, {r1-r12, r14}
         bx lr      ; Return to the C program    
+
+crct    sub r5, r5, #1          ; correct the error
+        eor r0, r0, #1, lsl r5
+        bic r1, r0, #0xfb
+        bic r2, r0, #0xef
+        bic r3, r0, #0xdf
+        bic r4, r0, #0xbf
+recon   mov r0, r1, lsr #2      ; reconstruct
+        add r0, r0, r2, lsr #3
+        add r0, r0, r3, lsl #3
+        add r0, r0, r5, lsl #3
+        b done
 
 div
         stmfd r13!, {r1-r12, r14}
+
+        ; assumes the flow chart provided in the division discussion is correct
 
         ; r0 - dividend
         ; r1 - divisor
