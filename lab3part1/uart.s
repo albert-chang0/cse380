c94291d76b6fdc66bb6dd9e0af5308b23c172811
Albert Chang <albert.chang@gmx.com>
2011-02-10 17:47:28 -0500

    Changes incorrect command mvo to mov

--- lab3part1/uart.s
+++ lab3part1/uart.s
@@ -1,7 +1,7 @@
     area    serial, code, readwrite 
     export lab3
     
-    u0lsr equ 0x14          ; UART0 line status register
+u0lsr equ 0x14          ; UART0 line status register
 
 lab3
         stmfd sp!,{lr}  ; Store register lr on stack
@@ -9,6 +9,7 @@ lab3
         ;bl read_character
         ; test code
         ;mov r0, #65
+        mov r0, #0
         bl output_character
 
         ldmfd sp!, {lr} ; Restore register lr from stack    
@@ -23,14 +24,14 @@ read_character
 output_character
         stmfd sp!, {r1-r12, lr}
 
-        mov r2, =0xe000c000 ; UART0 base address
+        ldr r2, =0xe000c000 ; UART0 base address
 
 poll    ldr r1, [r2, #u0lsr]    ; load status register
         and r1, r1, #0x20       ; test THRE in status register
         cmp r1, #0              ; poll until something is written
         beq poll
 
-        str r0, [r3]    ; write whatever is written to UART register
+        strb r0, [r2]    ; write whatever is written to UART register
 
         ldmfd sp!, {r1-r12, lr}
         bx lr
