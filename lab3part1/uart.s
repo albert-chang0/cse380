cb555f516156ccf10e24d4c23c5cc4c55a4e64b8
Albert Chang <albert.chang@gmx.com>
2011-02-10 10:34:21 -0500

    Adds general idea of how transmitting data via polling will work.

--- lab3part1/uart.s
+++ lab3part1/uart.s
@@ -1,7 +1,8 @@
     area    serial, code, readwrite 
     export lab3
     
-    u0lsr equ 0x14          ; UART0 Line Status Register
+    u0lsr equ 0x14          ; UART0 line status register
+    u0tr equ 0xe000c000    ; UART0 receive/tramsit hold register
 
     ; You'll want to define more constants to make your code easier 
     ; to read and debug
@@ -12,7 +13,7 @@ lab3
 
         ; bl read_character
         ; test code
-        mov r0, #65
+        ;mov r0, #65
         bl output_character
 
         ldmfd sp!, {lr} ; Restore register lr from stack    
@@ -26,6 +27,12 @@ read_character
 
 output_character
         stmfd sp!, {r1-r12, lr}
+
+poll    ldr r0  ; load status register
+                ; test THRE in status register
+                ; poll until something is written
+
+                ; something is written: store it to the uart
 
         ldmfd sp!, {r1-r12, lr}
         bx lr
