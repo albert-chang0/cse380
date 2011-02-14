b356c65080de9cf052571682fbe6240ffba39174
Albert Chang <albert.chang@gmx.com>
2011-02-14 13:59:59 -0500

    Changes comments, loading instructions to load byte, and registers being stored/restored.

--- lab3part1/uart.s
+++ lab3part1/uart.s
@@ -7,40 +7,40 @@ lab3
         stmfd sp!,{lr}  ; Store register lr on stack
 
         ;bl read_character
-        mov r0, #65
+        mov r0, #0x41
         bl output_character
 
         ldmfd sp!, {lr} ; Restore register lr from stack    
         bx lr
 
 read_character
-        stmfd sp!, {r1-r12, lr}
+        stmfd sp!, {r1, r2, lr}
 
-        ldr r2, =0xe000c000
+        ldr r2, =0xe000c000     ; UART0 base address
 
-rpoll   ldr r1, [r2, #u0lsr]    ; load status register
-        and r1, r1, #1          ; test RDR in status register
-        cmp r1, #0              ; poll until something needs to be read
-        beq rpoll
+rpoll   ldrb r1, [r2, #u0lsr]   ; load status register
+        and r1, r1, #1          ; test RDR
+        cmp r1, #0
+        beq rpoll               ; poll until something needs to be read
 
         ldrb r0, [r2]           ; read receiver buffer
 
-        ldmfd sp!, {r1-r12, lr}
+        ldmfd sp!, {r1, r2, lr}
         bx lr
 
 output_character
-        stmfd sp!, {r1-r12, lr}
+        stmfd sp!, {r1, r2, lr}
 
-        ldr r2, =0xe000c000 ; UART0 base address
+        ldr r2, =0xe000c000     ; UART0 base address
 
-tpoll   ldr r1, [r2, #u0lsr]    ; load status register
-        and r1, r1, #0x20       ; test THRE in status register
-        cmp r1, #0              ; poll until something is written
-        beq tpoll
+tpoll   ldrb r1, [r2, #u0lsr]   ; load status register
+        and r1, r1, #0x20       ; test THRE
+        cmp r1, #0
+        beq tpoll               ; poll until ready to write
 
-        strb r0, [r2]    ; write whatever is written to UART register
+        strb r0, [r2]           ; write to UART register
 
-        ldmfd sp!, {r1-r12, lr}
+        ldmfd sp!, {r1, r2, lr}
         bx lr
 
         end
