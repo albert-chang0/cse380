4cb306760d94fb08bf7ae347d8468c69521fe4d5
Albert Chang <albert.chang@gmx.com>
2011-02-14 18:17:40 -0500

    Copies character routines from lab3part1; adds skeleton code and comments for the other functions; configures and enables UART in assembly file.

--- lab3part2/mod_ui.s
+++ lab3part2/mod_ui.s
@@ -1,26 +1,152 @@
-	AREA	Serial, CODE, READWRITE	
-	EXPORT lab3
-	
-	U0LSR EQU 0x14			; UART0 Line Status Register
-
-		; You'll want to define more constants to make your code easier 
-		; to read and debug
-	   
-		; Memory allocated for user-entered strings
+    area    serial, code, readwrite 
+    export lab3
+    
+pinsel0 equ 0xe002c000  ; UART0 pin select
+u0base equ 0xe000c000   ; UART0 base address
+u0lsr equ 0x14          ; UART0 line status register
+u0lcr equ 0xc           ; UART0 line control register
+u0dlm equ 0x4           ; UART0 divisor latch MSB register
+                        ; UART0 divisor latch LSB register has no offset
 
 prompt = "Enter a number:  ",0          
-
-		; Additional strings may be defined here
-
-	ALIGN
-
+        align
 
 lab3
-	STMFD SP!,{lr}	; Store register lr on stack
+        stmfd sp!,{lr}  ; Store register lr on stack
 
-		; Your code is placed here
+        bl uart_init
 
-	LDMFD SP!, {lr}	; Restore register lr from stack	
-	BX lr
+        ldmfd sp!, {lr} ; Restore register lr from stack    
+        bx lr
 
-	END
+; uart_init
+; parameters: none
+; returns: none
+;
+; Enables and configures the UART we are going to use (UART0).
+; Basically a translation of the function serial_init() in mod_ui_wrapper.c.
+uart_init
+        stmfd sp!, {r0, r1, lr}
+
+        ldr r1, =pinsel0
+
+        ; enable UART0
+        ldr r0, [r1]
+        bic r0, r0, #0xf
+        orr r0, r0, #0x5
+        str r0, [r1]
+
+        ldr r1, =u0base
+
+        ; configurations:
+        ; 8-bit word length
+        ; 1 stop bit
+        ; no parity
+        ; disable break control
+        ; enable latch access
+        mov r0, #0x83
+        strb r0, [r1, #u0lcr]
+
+        ; 9600 baud rate:
+        mov r0, #0x78
+        strb r0, [r1]
+        mov r0, #0
+        strb r0, [r1, #u0dlm]
+
+        ; disable latch access
+        mov r0, #3
+        strb r0, [r1, #u0lcr]
+
+        ldmfd sp!, {r0, r1, lr}
+        bx lr
+
+; output_character
+; parameters:
+;     r0 - character to output to UART
+; returns: none
+;
+; Taken from the first part of lab3, this accepts a parameter from register
+; r0, and outputs it to UART.
+output_character
+        stmfd sp!, {r1, r2, lr}
+
+        ldr r2, =u0base         ; UART0 base address
+
+tpoll   ldrb r1, [r2, #u0lsr]   ; load status register
+        and r1, r1, #0x20       ; test THRE
+        cmp r1, #0
+        beq tpoll               ; poll until ready to write
+
+        strb r0, [r2]           ; write to UART register
+
+        ldmfd sp!, {r1, r2, lr}
+        bx lr
+
+; read_character
+; parameters: none
+; returns:
+;     r0 - character read in from UART
+;
+; Taken from the first part of lab3, this reads from UART and returns it in
+; register r0.
+read_character
+        stmfd sp!, {r1, r2, lr}
+
+        ldr r2, =u0base         ; UART0 base address
+
+rpoll   ldrb r1, [r2, #u0lsr]   ; load status register
+        and r1, r1, #1          ; test RDR
+        cmp r1, #0
+        beq rpoll               ; poll until something needs to be read
+
+        ldrb r0, [r2]           ; read receiver buffer
+
+        ldmfd sp!, {r1, r2, lr}
+        bx lr
+
+; output_string
+; parameters:
+;     r0 - base address of string to be printed
+; returns: none
+;
+; Displays a null terminated string, which the base address is passed in
+; through register r0, to UART.
+output_string
+        stmfd sp!, {lr}
+
+
+        ldmfd sp!, {lr}
+        bx lr
+
+; read_string
+; parameters: none
+; returns
+;     r0 - base address of the string being stored
+;
+; Reads in a string from UART and stores the base address in register r0.
+read_string
+        stmfd sp!, {lr}
+
+
+        ldmfd sp!, {lr}
+        bx lr
+
+; mod
+; parameters:
+;     r0 - divisor
+;     r1 - dividened
+; returns:
+;     r0 - modulo of r0 and r1 (r1 % r0)
+;
+; Performs a modulo (remainder of a divide function) on the parameters
+; passed through registers r0 and r1. The function should follow:
+; r1 modulo r0 (r1 % r0)
+; The majority of the function is taken from lab 2's division subroutine.
+mod
+        stmfd sp!, {lr}
+
+
+        ldmfd sp!, {lr}
+        bx lr
+
+        end
