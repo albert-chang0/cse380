d1d2b5ff3c141ab087ec659d0bc9ed4de60e6129
Albert Chang <albert.chang@gmx.com>
2011-03-02 09:23:37 -0500

    Changes prompt in lab 5; adds additional variables; comment routine purposes.

--- lab5/interrupts_test.s
+++ lab5/interrupts_test.s
@@ -1,23 +1,66 @@
     area interrupts, code, readwrite
+
     export lab5
     export FIQ_Handler
 
-prompt = "Welcome to lab #5",0
-    align
-pinsel0 equ 0xe002c000
-extint equ 0xe01fc140
-extmode equ 0x8
-vicbaseaddr equ 0xfffff000
-vicintenable equ 0x10
-vicintselect equ 0xc
+    extern uart_init
+    extern output_character
+    extern read_character
+    extern output_string
+    extern read_string
+    extern display_digit
 
+pinsel0 equ 0xe002c000      ; pin select
+iobase equ 0xe0028000
+io0dir equ 0x8
+extint equ 0xe01fc140       ; external interrupt flag
+extmode equ 0x8             ; external interrupt mode control
+vicbaseaddr equ 0xfffff000  ; vic base address
+vicintenable equ 0x10       ; interrupt enable
+vicintselect equ 0xc        ; select fiq or irq
+
+prompt = "Welcome to Interrupt Test!",0
+    align
+
+; lab5
+; parameters: none
+; returns: none
+;
+; Reads hexadecimal from UART0, and illuminates digit on 7-segment display.
+; Keep display illuminated until another digit is entered or user presses the
+; user interrupt button, which turns off the display. All events handled by
+; interrupts.
 lab5        
         stmfd sp!, {lr}
 
+        ; setup uart and fast interrupts
+        bl uart_init
+        bl interrupt_init
+
+        ; setup gpio, makes sure ports 0.7-0.13 are for gpio by zeroing them.
+        ldr r0, =pinsel0
+        ldr r1, [r0]
+        bic r1, r1, #0xff00000
+        bic r1, r1, #0xfc000
+        str r1, [r0]
+
+        ; setup direction
+        ; P0.7-P0.13, output 7-segment
+        ldr r0, =iobase
+        ldr r1, [r0, #io0dir]
+        orr r1, r1, #0x260000
+        orr r1, r1, #0x3f80
+        str r1, [r0, #io0dir]
 
         ldmfd sp!,{lr}
         bx lr
 
+; interrupt_init
+; parameters: none
+; returns: none
+;
+; Enables and configures fast interrupts, disables normal interrupt.
+; Uses external interrupt 1 and classifies it as a fast interrupt.
 interrupt_init       
         stmfd sp!, {r0, r1, lr}         ; Save registers 
 
@@ -55,6 +98,11 @@ interrupt_init
         ldmfd sp!, {r0, r1, lr}         ; Restore registers
         bx lr                           ; Return
 
+; FIQ_Handler
+; parameters: none
+; returns: none
+;
+; Fast interrupt handler
 FIQ_Handler
         stmfd sp!, {r0-r12, lr}         ; Save registers 
 
